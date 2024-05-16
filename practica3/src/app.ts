2a84538e2a70ed37079de247dbae8e25e5b08693
AdonisVelez <aavc.1316765427@gmail.com>
2024-05-16 16:11:56 -0500

    Practica #3

--- practica3/src/app.ts
+++ practica3/src/app.ts
@@ -8,8 +8,14 @@ app.use(express.json());
 // Montar las rutas
 app.use('/api', routes);
 
+// Ruta de bienvenida
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a la aplicación!');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
